2e2c4eb46495cbb5ee3d36a29a34317ef8e8f9f7
jkaplanis <johntkaplanis@gmail.com>
2020-05-08 18:41:33 -0700

    Updates

---
+++ precache-manifest.4231b786026f6b606f46f34652b962e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16d7565c22f2b0ab678bf90678221b6",
+    "url": "/react-user-directory/index.html"
+  },
+  {
+    "revision": "9ed9375b97875d115eb6",
+    "url": "/react-user-directory/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "e854d63dc89f0ecfb294",
+    "url": "/react-user-directory/static/css/main.b3ea1031.chunk.css"
+  },
+  {
+    "revision": "9ed9375b97875d115eb6",
+    "url": "/react-user-directory/static/js/2.d99a7b7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-user-directory/static/js/2.d99a7b7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e854d63dc89f0ecfb294",
+    "url": "/react-user-directory/static/js/main.f3dda138.chunk.js"
+  },
+  {
+    "revision": "b20634e4c0f3e78fc327",
+    "url": "/react-user-directory/static/js/runtime-main.c1b9af73.js"
+  }
+]);
